4cf90d2c70e3d931e404d637d1ffbafde909cfb8
sdim-ua <ingersol@yandex.ua>
2018-12-18 00:49:43 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,20 @@
+/* 
+Откуда берутся Person?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Person person = new Person();
+        
+        //напишите тут ваш код
+    }
+
+    public static class Person {
+        String name;
+        int age;
+        int weight;
+        int money;
+        
+        //напишите тут ваш код
+    }
+}
